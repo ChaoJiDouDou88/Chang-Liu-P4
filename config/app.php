b7aa48c8d702ed3a23be063db111496b642114d3
Chang Liu <liuchang39@hotmail.com>
2015-11-23 22:27:07 -0800

    Add database debug route

--- config/app.php
+++ config/app.php
@@ -145,6 +145,8 @@ return [
         p4\Providers\EventServiceProvider::class,
         p4\Providers\RouteServiceProvider::class,
 
+        Barryvdh\Debugbar\ServiceProvider::class,
+
     ],
 
     /*
@@ -193,6 +195,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        'Debugbar'  => Barryvdh\Debugbar\Facade::class,
 
     ],
 
